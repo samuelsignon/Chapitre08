58167b8a4d390a4ac7a501b049cbce8100358561
Signon Prince Samuel <samuelsignon22@gmail.com>
2026-05-28 11:10:32 +0000

    Methode Add() ajouté.

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,6 +1,7 @@
 package bookstoread;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,6 @@ public class BookShelf {
     }
 
     public void add(String bookToAdd) {
-        books.add(bookToAdd);
+       books.addAll(Arrays.asList(bookToAdd));
     }
 }
